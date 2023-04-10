4b713c8335a3638b28177459617163d9311e3f52
Badamasi Bello <79100208+BADAMASI462@users.noreply.github.com>
2023-04-10 11:39:04 +0100

    encoding categorical data

--- importing_libraries_&_reading_csv_&_creating_variables.py
+++ importing_libraries_&_reading_csv_&_creating_variables.py
@@ -5,6 +5,12 @@ import pandas as pd
 # BEST LIBRARY USED WITH DATA SCIENCE
 # WILL BE USED TO FILL IN MISSING DATA
 from sklearn.impute import SimpleImputer # AN OBJECT OF THIS CLASS WILL BE CREATED TO FILL IN MISSING DATA
+# LIBRARY TO ENCODE STRINGS(CATEGORICAL DATA). THE CLASS BELOW IS USED IN CONJUCTION WITH THE CLASS BELOW IT TO DO THE
+# ONEHOTENCODER
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+# LIBRARY TO ENCODE STRINGS(CATEGORICAL DATA). LABELENCODER
+from sklearn.preprocessing import LabelEncoder
 
 # READING OF CSV FILES MUST BE DONE WITH A RAW STRING r IN ORDER TO READ \SLASH OTHER WISE USE /
 
@@ -29,3 +35,13 @@ imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
 
 print(x)
+# ENCODING COUNTRIES WITH MORETHAN 2 CATEGORICAL DATA
+encoder = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])], remainder='passthrough')
+x = np.array(encoder.fit_transform(x))
+print(x)
+# ENCODING COLUMN WITH 2 CATEGORICAL DATA
+labelEncoder = LabelEncoder()
+y = labelEncoder.fit_transform(y)
+print(y)
+
+
