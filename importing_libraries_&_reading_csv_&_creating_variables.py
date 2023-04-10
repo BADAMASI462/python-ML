79c2dfdd35401cd345d687b5a1c71721703e5449
Badamasi Bello <79100208+BADAMASI462@users.noreply.github.com>
2023-04-10 11:14:21 +0100

    Filling missing data

--- importing_libraries_&_reading_csv_&_creating_variables.py
+++ importing_libraries_&_reading_csv_&_creating_variables.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 # MUST IMPORT PANDAS TO READ CSV FILES
 import pandas as pd
+# BEST LIBRARY USED WITH DATA SCIENCE
+# WILL BE USED TO FILL IN MISSING DATA
+from sklearn.impute import SimpleImputer # AN OBJECT OF THIS CLASS WILL BE CREATED TO FILL IN MISSING DATA
 
 # READING OF CSV FILES MUST BE DONE WITH A RAW STRING r IN ORDER TO READ \SLASH OTHER WISE USE /
 
-dataset = pd.read_csv(r"C:\Users\badamasi\Downloads\Machine Learning-A-Z-Codes-Datasets\Machine Learning A-Z (Codes and Datasets)\Part 1 - Data Preprocessing\Section 2 -------------------- Part 1 - Data Preprocessing --------------------\Python\Data.csv")
+dataset = pd.read_csv(r"C:\Users\badamasi\Desktop\Machine Learning-A-Z-Codes-Datasets\Machine Learning A-Z (Codes and Datasets)\Part 1 - Data Preprocessing\Section 2 -------------------- Part 1 - Data Preprocessing --------------------\Python\Data.csv")
 
 #ILOC FUNCTION CALLED INDEX LOCATION TO READ INDEXES IN ROWS.
 #THE RANGE IS USED TO READ ALL ROWS MEANING ALL FIELDS AS IN THIS CODE BELOW, 
@@ -17,3 +20,12 @@ y = dataset.iloc[:,-1].values
 
 print(x)
 print(y)
+
+# OBJECT OF THIS CLASS simpleImputer() CREATED
+imputer = SimpleImputer(missing_values=np.nan , strategy='mean')
+# THIS METHOD WILL CHANGE THE MISSING VALUES OF THE GIVES COLUMNS BY DETECTING nan AND REPLACING
+imputer.fit(x[:,1:3])
+# THIS METHOD WILL RETURN THE VALUES TO THE VARIABLE. PRECISELY TO THE COLUMNS OF WHICH THERE ARE MISSING DATA
+x[:,1:3] = imputer.transform(x[:,1:3])
+
+print(x)
