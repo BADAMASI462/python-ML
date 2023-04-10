93bd50e1c8ee2338719971b7eccb93cf547f3255
Badamasi Bello <79100208+BADAMASI462@users.noreply.github.com>
2023-04-10 13:40:25 +0100

    simple linear regression 1

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -20,7 +20,7 @@ from sklearn.linear_model import LinearRegression
 
 # READING OF CSV FILES MUST BE DONE WITH A RAW STRING r IN ORDER TO READ \SLASH OTHER WISE USE /
 
-dataset = pd.read_csv(r"C:\Users\badamasi\Desktop\Machine Learning-A-Z-Codes-Datasets\Machine Learning A-Z (Codes and Datasets)\Part 1 - Data Preprocessing\Section 2 -------------------- Part 1 - Data Preprocessing --------------------\Python\Data.csv")
+dataset = pd.read_csv(r"C:\Users\badamasi\Desktop\Machine Learning-A-Z-Codes-Datasets\Machine Learning A-Z (Codes and Datasets)\Part 2 - Regression\Section 4 - Simple Linear Regression\Python\Salary_Data.csv")
 
 #ILOC FUNCTION CALLED INDEX LOCATION TO READ INDEXES IN ROWS.
 #THE RANGE IS USED TO READ ALL ROWS MEANING ALL FIELDS AS IN THIS CODE BELOW, 
@@ -33,26 +33,10 @@ y = dataset.iloc[:,-1].values
 print(x)
 print(y)
 
-# OBJECT OF THIS CLASS simpleImputer() CREATED
-imputer = SimpleImputer(missing_values=np.nan , strategy='mean')
-# THIS METHOD WILL CHANGE THE MISSING VALUES OF THE GIVES COLUMNS BY DETECTING nan AND REPLACING
-imputer.fit(x[:,1:3])
-# THIS METHOD WILL RETURN THE VALUES TO THE VARIABLE. PRECISELY TO THE COLUMNS OF WHICH THERE ARE MISSING DATA
-x[:,1:3] = imputer.transform(x[:,1:3])
-
-print(x)
-# ENCODING COUNTRIES WITH MORETHAN 2 CATEGORICAL DATA
-encoder = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])], remainder='passthrough')
-x = np.array(encoder.fit_transform(x))
-print(x)
-# ENCODING COLUMN WITH 2 CATEGORICAL DATA
-labelEncoder = LabelEncoder()
-y = labelEncoder.fit_transform(y)
-print(y)
 
 # SPLITTING DATASET INTO TRAIN AND TEST
 # TEXT AND TRAIN VARIABLES MUST BE ARRANGED IN THIS WAY
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,shuffle=False)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=1/3,random_state=0)
 print(x_train)
 print(x_test)
 print(y_train)
@@ -61,10 +45,11 @@ print(y_test)
 # MODEL BUILDING
 simpleLinearRegression = LinearRegression()
 simpleLinearRegression.fit(x_train,y_train)
-simpleLinearRegression.predict(x_test)
+x_pred=simpleLinearRegression.predict(x)
 
-plt.scatter(x,y)
-plt.plot(x,simpleLinearRegression.predict(x) 
+plt.scatter(x_train,y_train,color='red')
+plt.plot(x_train,simpleLinearRegression.predict(x_train
+                                            ),color='blue'
 )
 plt.title('First graph'
           )
@@ -74,4 +59,15 @@ plt.xlabel('y'
            )
 plt.show()
 
+plt.scatter(x_test,y_test,color='red')
+plt.plot(x_train,simpleLinearRegression.predict(x_train
+                                            ),color='blue'
+)
+plt.title('predict graph'
+          )
+plt.xlabel('x'
+           )
+plt.xlabel('y'
+           )
+plt.show()
 
